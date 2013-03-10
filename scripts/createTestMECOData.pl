c9149d631093f8299f2903f3dcf7a3d0187de780
Daniel Zhang <daniel@danielzhang.info>
2013-03-09 18:14:21 -1000

    adding test data and test data creation script

---
+++ scripts/createTestMECOData.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+
+# With MECO export data, substitute values so they are meaningless.
+# This test data is used during unit testing.
+#
+# @author Daniel Zhang (張道博)
+
+use strict;
+use English qw(-no_match_vars);
+
+my $filename = $ARGV[$0];
+if ( !$filename ) {
+    print "Usage: createTestMECOData.pl \$FILENAME\n";
+    exit;
+}
+print STDERR "Processing $filename\n";
+open( FILE, "<$filename" );
+my @data = <FILE>;
+
+foreach my $line (@data) {
+    my $rand     = rand(5000);
+    my $origLine = $line;
+
+    if ( $line =~ /Summation=\"(.*)\"\s+/i ) {
+        $line = replaceCaptureGroup( $line, $rand );
+    }
+    if ( $line =~ /BlockEndValue=\"(.*)\"\//i ) {
+        $line = replaceCaptureGroup( $line, $rand );
+    }
+    if ( $line =~ /RawValue=\"(.*)\"\s+/i ) {
+        $line = replaceCaptureGroup( $line, $rand );
+    }
+    if ( $line =~ /CumulativeDemand=\"(.*)\"\s+/i ) {
+        $line = replaceCaptureGroup( $line, $rand );
+    }
+
+    print $line;
+}
+close(FILE);
+
+###
+# @param capture group string
+# @param replacement string
+# @return new string
+##
+sub replaceCaptureGroup {
+    my ( $string, $replacement ) = @_;
+    substr $string, $LAST_MATCH_START[1],
+        $LAST_MATCH_END[1] - $LAST_MATCH_START[1], $replacement;
+    return $string;
+}
