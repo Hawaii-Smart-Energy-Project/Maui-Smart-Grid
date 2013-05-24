de65b77672822e64b89c4b5d8f90b48b926d761b
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-05-23 14:47:22 -1000

    stub for plot test

---
+++ src/tests/test____mecoplotting.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Daniel Zhang (張道博)'
+
+import unittest
+from mecoplotting import MECOPlotting
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+
+class TestMECOPlotting(unittest.TestCase):
+
+    def setUp(self):
+        self.plotter = MECOPlotting()
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testPlot(self):
+        pass
