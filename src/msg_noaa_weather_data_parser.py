d153118279a3896416787eb3e495541f32800077
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-07-05 16:13:07 -1000

    converted to dict structure: #MSG-227 fixed

--- src/msg_noaa_weather_data_parser.py
+++ src/msg_noaa_weather_data_parser.py
@@ -5,6 +5,7 @@ __author__ = 'Daniel Zhang (張道博)'
 
 import csv
 import re
+from msg_logger import MSGLogger
 
 
 class MSGNOAAWeatherDataParser(object):
@@ -17,15 +18,36 @@ class MSGNOAAWeatherDataParser(object):
         """
         Constructor.
         """
-        pass
+        self.logger = MSGLogger(__name__, 'debug')
 
+        self.cols = ["wban", "datetime", "datetime", "station_type",
+                     "sky_condition", "sky_condition_flag", "visibility",
+                     "visibility_flag", "weather_type", "weather_type_flag",
+                     "dry_bulb_farenheit", "dry_bulb_farenheit_flag",
+                     "dry_bulb_celsius", "dry_bulb_celsius_flag",
+                     "wet_bulb_farenheit", "wet_bulb_farenheit_flag",
+                     "wet_bulb_celsius", "wet_bulb_celsius_flag",
+                     "dew_point_farenheit", "dew_point_farenheit_flag",
+                     "dew_point_celsius", "dew_point_celsius_flag",
+                     "relative_humidity", "relative_humidity_flag",
+                     "wind_speed", "wind_speed_flag", "wind_direction",
+                     "wind_direction_flag", "value_for_wind_character",
+                     "value_for_wind_character_flag", "station_pressure",
+                     "station_pressure_flag", "pressure_tendency",
+                     "pressure_tendency_flag", "pressure_change",
+                     "pressure_change_flag", "sea_level_pressure",
+                     "sea_level_pressure_flag", "record_type",
+                     "record_type_flag", "hourly_precip", "hourly_precip_flag",
+                     "altimeter", "altimeter_flag"]
 
     def parseWeatherData(self, fileObject, stationIDs):
         """
         :param fileObject: File object containing weather data.
         :param stationIDs: List of station IDs to be parsed.
-        :returns: List of lists containing parsed weather data.
+        :returns: List of dictionaries containing parsed weather data.
         """
+        self.logger.log('Data column count = %s' % len(self.cols), 'debug')
+
         rowNum = 0
         lastCol = 0
 
@@ -33,10 +55,11 @@ class MSGNOAAWeatherDataParser(object):
         self.data = []
 
         for row in reader:
-            rowData = []
+            rowDict = {}
+
             newDate = ''
 
-            # Handle the header row.
+            # Handle the header row and determine the last column.
             if rowNum == 0:
                 colNum = 0
                 for col in row:
@@ -50,7 +73,7 @@ class MSGNOAAWeatherDataParser(object):
 
                     if colNum == 0:
                         if self.stationShouldBeProcessed(col, stationIDs):
-                            pass
+                            rowDict['wban'] = col
                         else:
                             # Skip station IDs not marked for processing.
                             break
@@ -62,34 +85,45 @@ class MSGNOAAWeatherDataParser(object):
 
                     elif colNum == 2: # time column
                         time = col.zfill(4)
-                        rowData.append(
-                            '%s %s:%s' % (newDate, time[0:2], time[2:4]))
+                        rowDict['datetime'] = '%s %s:%s' % (
+                            newDate, time[0:2], time[2:4])
                         newDate = ''
                     else:
-                        rowData.append('%s' % col)
+                        try:
+                            rowDict[self.cols[colNum]] = '%s' % col
+                        except IndexError, e:
+                            print "Exception during first assignment: %s, " \
+                                  "Index = %s" % (
+                                      e, colNum)
                     colNum += 1
 
                     if colNum == lastCol:
                         for i in range(0, lastCol - 1):
 
                             try:
-                                if len(rowData[i]) == 0 or len(
-                                        re.sub("\s+", "", rowData[i])) == 0:
-                                    rowData[i] = 'NULL'
+                                if len(rowDict[self.cols[i]]) == 0 or len(
+                                        re.sub("\s+", "",
+                                               rowDict[self.cols[i]])) == 0:
+                                    rowDict[self.cols[i]] = 'NULL'
                                 else:
-                                    rowData[i] = "'" + rowData[i] + "'"
+                                    rowDict[self.cols[i]] = "'" + rowDict[
+                                        self.cols[i]] + "'"
                             except IndexError, e:
-                                assert rowData != [], "Data should never be " \
-                                                      "empty."
+                                print "Exception during second assignment: " \
+                                      "%s, Index = %s" % (
+                                          e, i)
 
-                        self.data.append(rowData)
+                        assert rowDict != {}, "Dict should never be empty."
+
+                        self.data.append(rowDict)
+
                         # End for col
 
             rowNum += 1
 
-        # End for row
+            # End for row
 
-        # print self.data
+        print self.data
         return self.data
 
 
