dda72b960020d87b5e443c05941ad0bc0930642e
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-07-12 17:40:19 -1000

    days are reported for weather loading: #MSG-230 fixed

--- src/insertCompressedNOAAWeatherData.py
+++ src/insertCompressedNOAAWeatherData.py
@@ -24,17 +24,19 @@ import gzip
 from msg_noaa_weather_data_parser import MSGNOAAWeatherDataParser
 from msg_weather_data_inserter import MSGNOAAWeatherDataInserter
 from meco_db_connector import MSGDBConnector
+from msg_time_util import MSGTimeUtil
 
 
 configer = MSGConfiger()
 logger = MSGLogger(__name__, 'info')
 binPath = MSGConfiger.configOptionValue(configer, "Executable Paths",
-                                         "bin_path")
+                                        "bin_path")
 commandLineArgs = None
 msgBody = ''
 notifier = MSGNotifier()
 dataParser = MSGNOAAWeatherDataParser()
 inserter = MSGNOAAWeatherDataInserter()
+timeUtil = MSGTimeUtil()
 
 
 def processCommandLineArguments():
@@ -73,7 +75,7 @@ if commandLineArgs.testing:
 else:
     databaseName = configer.configOptionValue("Database", "db_name")
 
-msg = "Recursively inserting weather data to the database named %s." % \
+msg = "Recursively inserting NOAA weather data to the database named %s." % \
       databaseName
 print msg
 msgBody += msg + "\n"
@@ -83,24 +85,33 @@ msg = "Starting in %s." % startingDirectory
 print msg
 msgBody += msg + "\n"
 
+allDays = []
+weatherDays = []
+setOfAllDays = set()
 for root, dirnames, filenames in os.walk('.'):
 
     for filename in fnmatch.filter(filenames, '*hourly.txt.gz'):
         fullPath = os.path.join(root, filename)
         msg = fullPath
         print msg
+        msgBody += "Processing %s.\n" % msg
         fileObject = gzip.open(fullPath, "rb")
-        inserter.insertDataDict(conn, 'WeatherNOAA',
-                                dataParser.parseWeatherData(fileObject,
-                                                            [KAHULUI_AIRPORT]),
-                                commit = True)
+        weatherDays = inserter.insertDataDict(conn, 'WeatherNOAA',
+                                              dataParser.parseWeatherData(
+                                                  fileObject,
+                                                  [KAHULUI_AIRPORT]),
+                                              commit = True)
+        allDays += weatherDays
+
         fileObject.close()
         if TESTING:
             break
 
+setOfAllDays = set(allDays)
+msgBody += timeUtil.reportOfDays(setOfAllDays)
+
 parseLog = ''
 
 if commandLineArgs.email:
-    # Provide loading notification with results by email.
-
-    pass
+    notifier.sendMailWithAttachments(msgBody, files = None,
+                                     testing = commandLineArgs.testing)
---
+++ src/msg_time_util.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Daniel Zhang (張道博)'
+
+from time import strptime
+import datetime as dt
+import sys
+from msg_logger import MSGLogger
+
+
+class MSGTimeUtil(object):
+    """
+    """
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+
+        self.logger = MSGLogger(__name__, 'debug')
+
+    def reportOfDays(self, datetimes = None):
+        """
+        Return report of days processed.
+
+        :param datetimes: A set of datetimes.
+
+        @todo Verify datetimes is a Set.
+        """
+
+        self.logger.log("datetimes = %s" % datetimes, 'debug')
+
+        if datetimes is None:
+            return "No days processed."
+
+        datetimeList = list(datetimes)
+        datetimeList.sort()
+
+        countOfDays = len(datetimeList)
+        firstDay = datetimeList[0]
+        lastDay = datetimeList[len(datetimeList) - 1]
+
+        if countOfDays == 1:
+            return "Processed 1 day with date %s." % (firstDay)
+        else:
+            return "Processed %s days between %s to %s, inclusive." % (
+                countOfDays, firstDay, lastDay)
+
+
+# For debugging
+timeUtil = MSGTimeUtil()
+
+testDays = set()
+testDays.add(dt.datetime.strptime("2003-01-01 00:00", "%Y-%m-%d %H:%M"))
+testDays.add(dt.datetime.strptime("2003-01-02 00:00", "%Y-%m-%d %H:%M"))
+testDays.add(dt.datetime.strptime("2003-01-01 00:00", "%Y-%m-%d %H:%M"))
+
+print timeUtil.reportOfDays(testDays)
