81ff6366f2d1fcb4d85d7995a96696cb537be8ea
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-10-08 17:11:19 -1000

    adding path creation

--- src/msg_egauge_service/installEgaugeAutomaticDataServices.pl
+++ src/msg_egauge_service/installEgaugeAutomaticDataServices.pl
@@ -9,16 +9,49 @@
 #
 # Usage:
 #
-# sudo ./installEgaugeAutomaticDataServices.pl
+#     sudo ./installEgaugeAutomaticDataServices.pl
 
 use strict;
 
-# @todo Make directories if they don't already exist.
+my $installRoot = "/usr/local/msg-egauge-service";
+my $binDest     = "/usr/local/msg-egauge-service/bin";
+my $libDest     = "/usr/local/lib/perl5";
+my $configDest  = "/usr/local/msg-egauge-service/config";
 
-my $binDest = "/usr/local/msg-egauge-service/bin";
-my $libDest = "/usr/local/lib/perl5";
-my $configDest = "/usr/local/msg-egauge-service/config";
+if ( !-e $installRoot && !-d $installRoot ) {
+    print "Making root directory at $installRoot.\n";
+    mkdir($installRoot);
+}
+
+if ( !-e $binDest && !-d $binDest ) {
+    print "Making bin directory at $binDest.\n";
+    mkdir($binDest);
+}
+
+if ( !-e $libDest && !-d $libDest ) {
+    print "Making lib directory at $libDest.\n";
+    mkdir($libDest);
+}
+
+if ( !-e $configDest && !-d $configDest ) {
+    print "Making config directory at $configDest.\n";
+    mkdir($configDest);
+}
+
+if (   ( !-e $installRoot && !-d $installRoot )
+    || ( !-e $binDest    && !-d $binDest )
+    || ( !-e $libDest    && !-d $libDest )
+    || ( !-e $configDest && !-d $configDest ) )
+{
+    print "ERROR: Not all install paths exist.\n";
+    exit;
+}
+
+print "Copying files...\n";
 
 `cp getEgaugeData.pl $binDest`;
 `cp insertEgaugeData.pl $binDest`;
 `cp DZSEPLib.pm $libDest`;
+
+print "Finished.\n";
+
