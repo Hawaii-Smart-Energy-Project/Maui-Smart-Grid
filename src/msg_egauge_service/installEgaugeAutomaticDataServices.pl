b470d94dcdb520e3567c764ee369f364f73b76d2
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-10-21 15:50:52 -1000

    installer can be used from any path now

--- src/msg_egauge_service/installEgaugeAutomaticDataServices.pl
+++ src/msg_egauge_service/installEgaugeAutomaticDataServices.pl
@@ -1,18 +1,21 @@
 #!/usr/bin/perl -w
 
-# Installation script for
-# eGauge Automatic Data Services
+# Installation script for MSG eGauge Automatic Data Services
 #
 # @author Daniel Zhang (張道博)
+#
 # @copyright Copyright (c) 2013, University of Hawaii Smart Energy Project
 # @license https://raw.github.com/Hawaii-Smart-Energy-Project/Maui-Smart-Grid/master/BSD-LICENSE.txt
 #
 # Usage:
 #
-#     sudo ./installEgaugeAutomaticDataServices.pl
+#     sudo perl installEgaugeAutomaticDataServices.pl
 
 use strict;
+use Cwd qw(abs_path getcwd);
+use File::Basename;
 
+my $sourcePath  = dirname( abs_path($0) );
 my $installRoot = "/usr/local/msg-egauge-service";
 my $binDest     = "/usr/local/msg-egauge-service/bin";
 my $libDest     = "/usr/local/lib/perl5";
@@ -47,6 +50,10 @@ if (   ( !-e $installRoot && !-d $installRoot )
     exit;
 }
 
+print "Installing from $sourcePath.\n";
+
+chdir($sourcePath) or die "Cannot change to $sourcePath.\n";
+
 print "Copying files...\n\n";
 
 my $cmd;
@@ -63,3 +70,4 @@ foreach (@cmds) {
 }
 
 print "\nFinished.\n";
+
