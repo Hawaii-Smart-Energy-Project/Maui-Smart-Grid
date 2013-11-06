9c3e5b5c93b5a87bc21b8866ff64bf4a67dde2a7
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-11-06 13:16:44 -1000

    adding logger tests: init, log recording

--- test/test____msg_logger.py
+++ test/test____msg_logger.py
@@ -4,12 +4,30 @@
 __author__ = 'Daniel Zhang (張道博)'
 
 import unittest
+from msg_logger import MSGLogger
+from logging import INFO
+from logging import DEBUG
+from logging import ERROR
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class MSGLoggerTester(unittest.TestCase):
 
+    def setUp(self):
+        self.logger = MSGLogger(__name__)
+
+    def testInit(self):
+        self.assertIsNotNone(self.logger)
+
+    def testLogRecording(self):
+        msg = "Recording test."
+
+        self.logger.startRecording()
+        self.logger.log(msg, INFO)
+        self.logger.endRecording()
+        self.logger.log("This should not be logged.", INFO)
+
+        self.assertEqual(self.logger.recording, msg)
+        
 
 if __name__ == '__main__':
     unittest.main()
