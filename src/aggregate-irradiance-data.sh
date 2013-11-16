a3a04a4260fa6d035cb9733394d119534b5f8b4c
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-11-15 23:16:23 -1000

    adding aggregated data generator script

---
+++ src/aggregate-irradiance-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Generate aggregated irradiance data in half-year portions.
+#
+# @author Daniel Zhang (張道博)
+
+time python aggregateIrradianceData.py --startDate "2012-01-01 00:00:00" --endDate "2012-06-30 11:45:00" > avg-irradiance-15-min-2012-first-half.csv
+time python aggregateIrradianceData.py --startDate "2012-06-30 11:45:00" --endDate "2013-01-01 00:00:00" > avg-irradiance-15-min-2012-second-half.csv
+time python aggregateIrradianceData.py --startDate "2013-01-01 00:00:00" --endDate "2013-06-30 11:59:59" > avg-irradiance-15-min-2013-first-half.csv
+time python aggregateIrradianceData.py --startDate "2013-06-30 11:45:00" --endDate "2014-01-01 00:00:00" > avg-irradiance-15-min-2013-second-half.csv
