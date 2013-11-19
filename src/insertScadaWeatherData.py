4d2c5706cb0901e80b89033d5d0e7a466ee6aa80
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-11-18 22:54:40 -1000

    insert was successful over 4 months

--- src/insertScadaWeatherData.py
+++ src/insertScadaWeatherData.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-One-off script for inserting Kihei SCADA temperature and humidity data.
+Script for inserting Kihei SCADA temperature and humidity data.
 
 Usage:
 
 With the current working directory set to the path containing the data files,
 
     python insertSCADAWeatherData.py
-    
+
 """
 
 __author__ = 'Daniel Zhang (張道博)'
@@ -34,9 +34,6 @@ tFiles = ['Kihei AirTemp F 2013_07.csv', 'Kihei AirTemp F 2013_08.csv',
 hFiles = ['Kihei_Rel_Humid 2013_07.csv', 'Kihei_Rel_Humid 2013_08.csv',
           'Kihei_Rel_Humid 2013_09.csv', 'Kihei_Rel_Humid 2013_10.csv']
 
-tFiles = ['Kihei AirTemp F 2013_07.csv']
-hFiles = ['Kihei_Rel_Humid 2013_07.csv']
-
 table = 'KiheiSCADATemperatureHumidity'
 tCols = ['timestamp', 'met_air_temp_degf']
 hCols = ['met_rel_humid_pct']
@@ -47,7 +44,7 @@ temps = []
 t_i = 0
 h_i = 0
 
-for i in range(1):
+for i in range(4):
 
     with open(tFiles[i], 'rb') as csvfile:
         print "Reading %s" % (tFiles[i])
@@ -62,6 +59,9 @@ for i in range(1):
 
             row[0] = row[0].replace('GMT-1000', '')
             row[0] = re.sub(r'\s24:', '\s00:', row[0])
+
+            if row[1] == '':
+                row[1] = 'NULL'
 
             sql = """INSERT INTO "%s" (%s) VALUES (TIMESTAMP %s,%s)""" % (
                 table, ','.join(tCols), "'" + row[0].strip() + "'",
@@ -90,14 +90,19 @@ for i in range(1):
                 continue
 
             row[0] = row[0].replace('GMT-1000', '')
+            row[0] = re.sub(r'\s24:', '\s00:', row[0])
 
-            sql = """UPDATE "%s" SET (%s) VALUES (%s) WHERE timestamp =
+            if row[1] == '':
+                row[1] = 'NULL'
+
+            sql = """UPDATE "%s" SET %s = %s WHERE timestamp =
             TIMESTAMP '%s'""" % (
-                table, ','.join(hCols), "'" + row[1].strip() + "'", row[0])
+                table, ','.join(hCols), "'" + row[1].strip() + "'",
+                row[0].strip())
 
             sql = sql.replace("'NULL'", 'NULL')
 
-            print sql
+            #print sql
             dbUtil.executeSQL(cursor, sql)
 
             cnt += 1
