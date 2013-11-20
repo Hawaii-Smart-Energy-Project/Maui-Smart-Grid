3934c07866f3119ebbf6ac7722370836bdf7b6eb
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-11-19 22:08:46 -1000

    adding src init

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Init file for the Maui Smart Grid src directory.
+"""
+
+__author__ = 'Daniel Zhang (張道博)'
+__copyright__ = 'Copyright (c) 2013, University of Hawaii Smart Energy Project'
+__license__ = 'https://raw.github' \
+              '.com/Hawaii-Smart-Energy-Project/Maui-Smart-Grid/master/BSD' \
+              '-LICENSE.txt'
