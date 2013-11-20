262afb0e10324f21003eec684446866a6f81eca7
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-11-19 16:14:59 -1000

    added math utils

---
+++ src/msg_math_util.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Daniel Zhang (張道博)'
+__copyright__ = 'Copyright (c) 2013, University of Hawaii Smart Energy Project'
+__license__ = 'https://raw.github' \
+              '.com/Hawaii-Smart-Energy-Project/Maui-Smart-Grid/master/BSD' \
+              '-LICENSE.txt'
+
+from msg_logger import MSGLogger
+
+
+class MSGMathUtil(object):
+    """
+    Math related utility methods.
+    """
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        self.logger = MSGLogger(__name__)
+
+    def isNumber(self, s):
+        try:
+            float(s)
+            return True
+        except (TypeError, ValueError):
+            return False
