ac31d0f2ef7a878675c7f74d76144c84f4ba2f2e
Daniel Zhang (張道博) <daniel@danielzhang.info>
2013-11-22 00:18:35 -1000

    modifying counting view to use "private" view which is then used in the public view

--- sql/table-creation/create_irradiance_intervals_with_sensor_ids.sql
+++ sql/table-creation/create_irradiance_intervals_with_sensor_ids.sql
@@ -7,79 +7,197 @@
 -- Create aggregated temperature view using NOAA weather data.
 --
 -- @author Daniel Zhang (張道博)
+DROP VIEW "public"."_dz_avg_irradiance_uniform_fifteen_min_intervals_null_as_zero";
 
-DROP VIEW "public"."dz_irradiance_uniform_15_min_intervals_null_as_zero";
 DROP TABLE "_IrradianceFifteenMinIntervals" CASCADE;
 
 CREATE TABLE "_IrradianceFifteenMinIntervals" AS (
-        WITH intervals AS (
-            SELECT
-                (
-                    (
-                        SELECT
-                (
-                    MIN("IrradianceData"."timestamp")
-                ) :: DATE AS MIN
-                        FROM
-                            "IrradianceData"
-                    ) + ((n.n || ' minutes' :: TEXT)) :: INTERVAL
-                ) AS start_time,
-                (
-                    (
-                        SELECT
-                (
-                    MIN("IrradianceData"."timestamp")
-                ) :: DATE AS MIN
-                        FROM
-                            "IrradianceData"
-                    ) + (((n.n + 15) || ' minutes' :: TEXT)) :: INTERVAL
-                ) AS end_time
-            FROM
-                generate_series(
-                    0,
-                    (
-                        (
-                            SELECT
-                                MAX("IrradianceData"."timestamp") :: DATE -
-                                MIN("IrradianceData"."timestamp") :: DATE
-                            FROM
-                                "IrradianceData"
-                        ) + 366
-                    ) * 24 * 60,
-                    15
-                ) n (n)
-        ) SELECT
-            generate_series(1,
-                            4) AS sensor_id,
-            *
-        FROM
-            intervals);
+	WITH intervals AS (
+		SELECT
+			(
+				(
+					SELECT
+						(
+							MIN ("IrradianceData"."timestamp")
+						) :: DATE AS MIN
+					FROM
+						"IrradianceData"
+				) + ((n.n || ' minutes' :: TEXT)) :: INTERVAL
+			) AS start_time,
+			(
+				(
+					SELECT
+						(
+							MIN ("IrradianceData"."timestamp")
+						) :: DATE AS MIN
+					FROM
+						"IrradianceData"
+				) + (((n.n + 15) || ' minutes' :: TEXT)) :: INTERVAL
+			) AS end_time
+		FROM
+			generate_series (
+				0,
+				(
+					(
+						SELECT
+							MAX ("IrradianceData"."timestamp") :: DATE - MIN ("IrradianceData"."timestamp") :: DATE
+						FROM
+							"IrradianceData"
+					) + 366
+				) * 24 * 60,
+				15
+			) n (n)
+	) SELECT
+		generate_series (1, 4) AS sensor_id,
+		*
+	FROM
+		intervals
+);
 
-CREATE VIEW "public"."dz_irradiance_uniform_15_min_intervals_null_as_zero" AS
-    SELECT
-        i.start_time,
-        AVG(
-            CASE
-            WHEN (
-                (w.dry_bulb_celsius) :: TEXT ~
-                '(([1-9]+\.[0-9]*)|([1-9]*\.[0-9]+)|([1-9]+))([eE][-+]?[0-9]+)?'
-                :: TEXT
-            ) THEN
-                (w.dry_bulb_celsius) :: DOUBLE PRECISION
-            ELSE
-                NULL :: DOUBLE PRECISION
-            END
-        ) AS AVG
-    FROM
-            "WeatherNOAA" w,
-            "_IrradianceFifteenMinIntervals" i
-    WHERE
-        (
-            date_trunc('hour' :: TEXT, i.start_time) =
-            date_trunc('hour' :: TEXT, w.datetime)
-        )
-    GROUP BY
-        i.start_time
-    ORDER BY
-        i.start_time;
-COMMENT ON VIEW "public"."dz_irradiance_uniform_15_min_intervals_null_as_zero" IS NULL;
+CREATE VIEW "public"."_dz_avg_irradiance_uniform_fifteen_min_intervals_null_as_zero" AS SELECT
+	"_IrradianceFifteenMinIntervals".end_time,
+	"_IrradianceFifteenMinIntervals".sensor_id,
+	CASE
+WHEN (
+	"AverageFifteenMinIrradianceData".irradiance_w_per_m2 IS NULL
+) THEN
+	(0.0) :: DOUBLE PRECISION
+ELSE
+	"AverageFifteenMinIrradianceData".irradiance_w_per_m2
+END AS irradiance_w_per_m2
+FROM
+	(
+		(
+			"_IrradianceFifteenMinIntervals"
+			LEFT JOIN "AverageFifteenMinIrradianceData" ON (
+				(
+					(
+						"_IrradianceFifteenMinIntervals".end_time = "AverageFifteenMinIrradianceData"."timestamp"
+					)
+					AND (
+						"_IrradianceFifteenMinIntervals".sensor_id = "AverageFifteenMinIrradianceData".sensor_id
+					)
+				)
+			)
+		)
+	)
+WHERE
+	(
+		(
+			"_IrradianceFifteenMinIntervals".end_time >= (
+				SELECT
+					date_trunc(
+						'day' :: TEXT,
+						MIN (
+							"AverageFifteenMinIrradianceData"."timestamp"
+						)
+					) AS date_trunc
+				FROM
+					"AverageFifteenMinIrradianceData"
+			)
+		)
+		AND (
+			"_IrradianceFifteenMinIntervals".end_time <= (
+				SELECT
+					date_trunc(
+						'day' :: TEXT,
+						MAX (
+							"AverageFifteenMinIrradianceData"."timestamp"
+						)
+					) AS date_trunc
+				FROM
+					"AverageFifteenMinIrradianceData"
+			)
+		)
+	)
+ORDER BY
+	"_IrradianceFifteenMinIntervals".end_time,
+	"_IrradianceFifteenMinIntervals".sensor_id;
+
+COMMENT ON VIEW "public"."_dz_avg_irradiance_uniform_fifteen_min_intervals_null_as_zero" IS NULL;
+
+CREATE VIEW "dz_count_of_fifteen_min_irradiance_intervals" AS SELECT
+	COUNT (*) / 4 AS cnt,
+	date_trunc('day', end_time) AS DAY
+FROM
+	"_dz_avg_irradiance_uniform_fifteen_min_intervals_null_as_zero"
+GROUP BY
+	DAY;
+
+DROP VIEW "public"."dz_avg_irradiance_uniform_fifteen_min_intervals_null_as_zero";
+
+CREATE VIEW "public"."dz_avg_irradiance_uniform_fifteen_min_intervals_null_as_zero" AS SELECT
+	"_IrradianceFifteenMinIntervals".end_time,
+	"_IrradianceFifteenMinIntervals".sensor_id,
+	CASE
+WHEN (
+	"AverageFifteenMinIrradianceData".irradiance_w_per_m2 IS NULL
+) THEN
+	(0.0) :: DOUBLE PRECISION
+ELSE
+	"AverageFifteenMinIrradianceData".irradiance_w_per_m2
+END AS irradiance_w_per_m2
+FROM
+	(
+		(
+			"_IrradianceFifteenMinIntervals"
+			LEFT JOIN "AverageFifteenMinIrradianceData" ON (
+				(
+					(
+						"_IrradianceFifteenMinIntervals".end_time = "AverageFifteenMinIrradianceData"."timestamp"
+					)
+					AND (
+						"_IrradianceFifteenMinIntervals".sensor_id = "AverageFifteenMinIrradianceData".sensor_id
+					)
+				)
+			)
+		)
+		JOIN dz_count_of_15_min_irradiance_intervals ON (
+			(
+				(
+					date_trunc(
+						'day' :: TEXT,
+						"_IrradianceFifteenMinIntervals".end_time
+					) = dz_count_of_fifteen_min_irradiance_intervals. DAY
+				)
+				AND (
+					dz_count_of_fifteen_min_irradiance_intervals.cnt = 96
+				)
+			)
+		)
+	)
+WHERE
+	(
+		(
+			"_IrradianceFifteenMinIntervals".end_time >= (
+				SELECT
+					date_trunc(
+						'day' :: TEXT,
+						MIN (
+							"AverageFifteenMinIrradianceData"."timestamp"
+						)
+					) AS date_trunc
+				FROM
+					"AverageFifteenMinIrradianceData"
+			)
+		)
+		AND (
+			"_IrradianceFifteenMinIntervals".end_time <= (
+				SELECT
+					date_trunc(
+						'day' :: TEXT,
+						MAX (
+							"AverageFifteenMinIrradianceData"."timestamp"
+						)
+					) AS date_trunc
+				FROM
+					"AverageFifteenMinIrradianceData"
+			)
+		)
+	)
+ORDER BY
+	"_IrradianceFifteenMinIntervals".end_time,
+	"_IrradianceFifteenMinIntervals".sensor_id;
+
+COMMENT ON VIEW "public"."dz_avg_irradiance_uniform_fifteen_min_intervals_null_as_zero" IS NULL;
+
